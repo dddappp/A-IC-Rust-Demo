30291afbd82cf0145c97859b365e329c7abbd3fb
yangjiefeng <yangjiefeng@gmail.com>
2023-09-07 17:52:13 +0800

    Initial commit.

---
+++ src/a_ic_rust_demo_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { a_ic_rust_demo_backend } from "../../declarations/a_ic_rust_demo_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await a_ic_rust_demo_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
