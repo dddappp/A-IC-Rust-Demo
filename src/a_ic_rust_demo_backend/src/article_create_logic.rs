1404071c5c3a6f06e7599d697a43ce7039cc75ec
yangjiefeng <yangjiefeng@gmail.com>
2023-09-26 21:13:45 +0800

    refactor

--- src/a_ic_rust_demo_backend/src/article_create_logic.rs
+++ src/a_ic_rust_demo_backend/src/article_create_logic.rs
@@ -8,14 +8,18 @@ pub(crate) fn verify(
     title: String,
     body: String,
 ) -> ArticleCreated {
-    ArticleCreated { article_id, title, body }
+    ArticleCreated {
+        article_id,
+        title,
+        body,
+    }
 }
 
 pub(crate) fn mutate(
     article_created: &ArticleCreated,
 ) -> Article {
     Article {
-        article_id: article_created.article_id,
+        article_id: article_created.article_id.clone(),
         version: 0,
         title: article_created.title.clone(),
         body: article_created.body.clone(),
